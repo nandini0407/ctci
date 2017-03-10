d78edda18bfbcd6db6ba8f6d8989015f32d70edf
nandini0407 <nandini0407@gmail.com>
2017-03-09 17:59:05 -0800

    add chap1 1.1

--- chap1_1.1.rb
+++ chap1_1.1.rb
@@ -1,3 +1,16 @@
 def is_unique?(str)
-  
+  counts = Hash.new
+
+  str.chars.each do |c|
+    if counts[c]
+      return false
+    else
+      counts[c] = 1
+    end
+  end
+
+  true
 end
+
+puts is_unique?('abcd')
+puts is_unique?('accd')
